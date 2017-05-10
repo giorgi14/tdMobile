8b3391619082bfb22451d2701cd78d29678305d3
adm <adm@adm-PC>
2017-05-10 15:32:49 +0400

    sdfgsdfsdf

--- includes/sendsmscron.php
+++ includes/sendsmscron.php
@@ -2,9 +2,9 @@
 require_once('classes/core.php');
 
 $user	   = $_SESSION['USERID'];
-$status    = 0;
 
-$result = mysqli_query("SELECT DATE_FORMAT(client_loan_schedule.pay_date, '%d.%m.%Y') AS `pay_date`,
+
+$result = mysql_query("SELECT DATE_FORMAT(client_loan_schedule.pay_date, '%d.%m.%Y') AS `pay_date`,
                                CONCAT(client_loan_schedule.pay_amount,IF(client_loan_agreement.loan_currency_id = 1, ' GEL', ' $')) AS `pay_amount`,
                         	   client.`name` AS cl_name,
                     		   client.id AS cl_id,
@@ -17,51 +17,48 @@ $result = mysqli_query("SELECT DATE_FORMAT(client_loan_schedule.pay_date, '%d.%m
                         JOIN   client_car ON client.id = client_car.client_id
                         WHERE  client_loan_schedule.actived = 1 AND client.actived = 1 
                         AND    client_loan_agreement.canceled_status = 0
-                        AND    DATEDIFF(client_loan_schedule.pay_date,CURDATE()) IN (SELECT sent_day.count FROM sent_day WHERE sent_day.actived = 1)");
+                        AND    DATEDIFF(client_loan_schedule.pay_date,CURDATE()) IN(SELECT sent_day.count FROM sent_day WHERE sent_day.actived = 1)");
 
 while ($row = mysql_fetch_array($result)) {
-$text = 'TG MOBILE GATKOBINEBT ('.$row[cl_car_info].') GADASAXDELIA MIMDINARE DAVALIANEBA '.$row[pay_date].'-SHI . TANXA '.$row[pay_amount].'. AUCILEBLAD MIUTITED XELSHEKRULEBIS  N '.$row[agr_id].' DA GADAXDIS RICXVI. IQONIET GADAXDIS QVITARI. SAQARTVELOS BANKI (S/K ‎205270277) GE12BG0000000523102000 (DAZGVEVIS GADAXDA GIWEVT  06.05.2017-shi).';
-$encodedtxt = urlencode($text);
-$check 		= file_get_contents('http://msg.ge/bi/sendsms.php?username=calldato1&password=di48fj47sh0&client_id=330&service_id=0330&to=995598777210&text=hello)');
-$check = 1;
-
+    
+   $result1 = mysql_query("SELECT  client_quarantors.id,
+                                   client_quarantors.phone
+                            FROM   client_quarantors 
+                            WHERE  client_id = $row[cl_id] 
+                            AND    client_quarantors.actived = 1 
+                            AND    client_quarantors.sms_sent = 1");
+   
+   $text = 'TG MOBILE GATKOBINEBT ('.$row[cl_car_info].') GADASAXDELIA MIMDINARE DAVALIANEBA '.$row[pay_date].'-SHI . TANXA '.$row[pay_amount].'. AUCILEBLAD MIUTITED XELSHEKRULEBIS  N '.$row[agr_id].' DA GADAXDIS RICXVI. IQONIET GADAXDIS QVITARI. SAQARTVELOS BANKI (S/K ‎205270277) GE12BG0000000523102000 (DAZGVEVIS GADAXDA GIWEVT  06.05.2017-shi).';
+   $encodedtxt = urlencode($text);
+   $check 		= file_get_contents('http://msg.ge/bi/sendsms.php?username=calldato1&password=di48fj47sh0&client_id=330&service_id=0330&to='.$row[phone].'&text='.$encodedtxt.')');
+   
+    while ($row1 = mysql_fetch_array($result1)) {
+        $check1 = file_get_contents('http://msg.ge/bi/sendsms.php?username=calldato1&password=di48fj47sh0&client_id=330&service_id=0330&to='.$row1[phone].'&text='.$encodedtxt.')');
+        
+        if($check1){
+            mysql_query("INSERT INTO `sent_sms`
+                                    (`user_id`, `datetime`, `client_id`, `guarantor_id`, `address`, `content`, `status`, `actived`)
+                              VALUES
+                                    ('$user', NOW(), '$row[cl_id]', '$row1[id]', '$row1[phone]', '$text', '1', '1')");
+        }else{
+            mysql_query("INSERT INTO `sent_sms`
+                                    (`user_id`, `datetime`, `client_id`, `guarantor_id`, `address`, `content`, `status`, `actived`)
+                              VALUES
+                                    ('$user', NOW(), '$row[cl_id]', '$row1[id]', '$row1[phone]', '$text', '1', '1')");
+        }
+    }
+    
     if($check){
-    	$status = 1;
-    	if ($id == '') {
-    	    mysql_query("INSERT INTO `sent_sms` 
-        					        (`user_id`, `datetime`, `client_id`, `address`, `content`, `status`, `actived`) 
-        		              VALUES 
-        					        ('$user', NOW(), '$row[cl_id]', '$row[phone]', '$text', '$status', '1')");
-    	}else{
-    	    mysql_query("UPDATE `sent_sms`
-                	        SET `user_id`   = '$user',
-    	                        `datetime`  =  NOW(),
-                    	        `client_id` = '$client_id',
-                    	        `address`   = '$sms_phone',
-                    	        `content`   = '$text',
-    	                        `status`    = '$status'
-                	     WHERE  `id`        = '$id'");
-    	}
-    	
+    	mysql_query("INSERT INTO `sent_sms` 
+    					        (`user_id`, `datetime`, `client_id`, `address`, `content`, `status`, `actived`) 
+    		              VALUES 
+    					        ('$user', NOW(), '$row[cl_id]', '$row[phone]', '$text', '1', '1')");
     }else{
-    	$status = 0;
-    	
-    	if ($id=='') {
-    	    mysql_query("INSERT INTO `sent_sms`
-                        	        (`user_id`, `datetime`, `client_id`, `address`, `content`, `status`, `actived`)
-                        	  VALUES
-                        	        ('$user', NOW(), '$client_id', '$sms_phone', '$text', '$status', '1')");
-    	}else{
-    	    mysql_query("UPDATE `sent_sms`
-            	            SET `user_id`   = '$user',
-    	                        `datetime`  =  NOW(),
-                    	        `client_id` = '$client_id',
-                    	        `address`   = '$sms_phone',
-                    	        `content`   = '$text',
-                    	        `status`    = '$status'
-            	         WHERE  `id`        = '$id'");
-    	}
-    	
+    	mysql_query("INSERT INTO `sent_sms`
+                    	        (`user_id`, `datetime`, `client_id`, `address`, `content`, `status`, `actived`)
+                    	  VALUES
+                    	        ('$user', NOW(), '$client_id', '$sms_phone', '$text', '0', '1')");
     }
 }
+
 ?>
