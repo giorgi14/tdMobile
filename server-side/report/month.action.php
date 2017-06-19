caad5a317b7f86ca5777cf15bece3dbc35315102
adm <adm@adm-PC>
2017-06-19 17:18:17 +0400

    sdfsdfsdfsdf

--- server-side/report/month.action.php
+++ server-side/report/month.action.php
@@ -12,13 +12,14 @@ switch ($action) {
 		$filt_month	= $_REQUEST['filt_month'];
 		$filt_year	= $_REQUEST['filt_year'];
 		$filt_month1 = $filt_month+1;
+		$filt_month2 = $filt_month+2;
 		$filt_day	= $_REQUEST['filt_day'];
 		$AND        = '';
 		if ($filt_day > 0) {
 		    $AND = "AND DAY(client_loan_agreement.datetime) ='$filt_day'";
 		}
 		
-		$rResult = mysql_query("SELECT MIN(client_loan_schedule.id),
+		$rResult = mysql_query("SELECT MAX(client_loan_schedule.id),
                             		   DATE_FORMAT(client_loan_agreement.datetime,'%d/%m/%Y') AS loan_date,
                                        IF(client_loan_agreement.no_standart = 1,CONCAT('<div title=\"არასტანდარტული ხელშეკრულება\" style=\"background: #009688;\">',IF(client.`name` = '',client.ltd_name,CONCAT(client.`name`,' ',client.lastname)), '</div>'),CONCAT(IF(client.`name` = '',client.ltd_name,CONCAT(client.`name`,' ',client.lastname)))) AS cl_name,
                             		   client_loan_agreement.oris_code,
@@ -38,11 +39,14 @@ switch ($action) {
                                 	   CASE
                                 		   WHEN client_loan_schedule.`status` = 1 AND client_loan_agreement.loan_currency_id = 1 THEN client_loan_schedule.remaining_root
                                            WHEN client_loan_schedule.`status` = 1 AND client_loan_agreement.loan_currency_id = 2 THEN ROUND(client_loan_schedule.remaining_root*client_loan_agreement.exchange_rate,2)
-                                           WHEN client_loan_schedule.`status` = 0 AND client_loan_agreement.loan_currency_id = 1 THEN client_loan_schedule.remaining_root+client_loan_schedule.root
-                                           WHEN client_loan_schedule.`status` = 0 AND client_loan_agreement.loan_currency_id = 2 THEN ROUND((client_loan_schedule.remaining_root+client_loan_schedule.root)*client_loan_agreement.exchange_rate,2)
+                                           WHEN client_loan_schedule.`status` = 0 AND client_loan_agreement.loan_currency_id = 1 THEN ROUND(client_loan_schedule.remaining_root + client_loan_schedule.root + (SELECT IFNULL(SUM(client_loan_schedule.percent),0) FROM client_loan_schedule WHERE client_loan_schedule.actived = 1 AND client_loan_schedule.`status` = 0 AND client_loan_schedule.client_loan_agreement_id = client_loan_agreement.id AND MONTH(client_loan_schedule.pay_date) <'$filt_month' AND YEAR(client_loan_schedule.pay_date)<='$filt_year'),2)
+                                           WHEN client_loan_schedule.`status` = 0 AND client_loan_agreement.loan_currency_id = 2 THEN ROUND((client_loan_schedule.remaining_root + client_loan_schedule.root + (SELECT IFNULL(SUM(client_loan_schedule.percent),0) FROM client_loan_schedule WHERE client_loan_schedule.actived = 1 AND client_loan_schedule.`status` = 0 AND client_loan_schedule.client_loan_agreement_id = client_loan_agreement.id AND MONTH(client_loan_schedule.pay_date) <'$filt_month' AND YEAR(client_loan_schedule.pay_date)<='$filt_year'))*client_loan_agreement.exchange_rate,2)
                                 	   END AS darchenili_vali,
                                        DATE_FORMAT(client_loan_schedule.schedule_date,'%d/%m/%Y') AS daricxvis_tarigi,
-                                	   IF(client_loan_schedule.schedule_date <= CURDATE(), IF(client_loan_agreement.loan_currency_id = 1, ROUND(client_loan_schedule.percent+IF(ISNULL(client_loan_schedule.penalty),0,client_loan_schedule.penalty),2),ROUND((client_loan_schedule.percent*client_loan_agreement.exchange_rate)+IF(ISNULL(client_loan_schedule.penalty),0,client_loan_schedule.penalty*client_loan_agreement.exchange_rate),2)),'0.00') AS percent,
+		                               IFNULL(CASE
+                                                 WHEN MONTH(client_loan_schedule.schedule_date) = '$filt_month' AND client_loan_schedule.schedule_date<=curdate() AND client_loan_agreement.loan_currency_id = 1 THEN ROUND(client_loan_schedule.percent+IF(client_loan_schedule.penalty>0 AND client_loan_schedule.`status` = 1,client_loan_schedule.penalty,0),2)
+                                                 WHEN MONTH(client_loan_schedule.schedule_date) = '$filt_month' AND client_loan_schedule.schedule_date<=curdate() AND client_loan_agreement.loan_currency_id = 2 THEN ROUND((client_loan_schedule.percent+IF(client_loan_schedule.penalty>0 AND client_loan_schedule.`status` = 1,client_loan_schedule.penalty,0))*client_loan_agreement.exchange_rate,2)
+                                              END,'0.00') AS percent,
                                       (SELECT DATE_FORMAT(money_transactions_detail.pay_datetime,'%d/%m/%Y')
                                        FROM   money_transactions
                                        JOIN   money_transactions_detail ON money_transactions.id = money_transactions_detail.transaction_id
@@ -50,7 +54,7 @@ switch ($action) {
                                        AND    money_transactions_detail.actived = 1
                                        AND    money_transactions_detail.`status` = 1
                                        AND    money_transactions_detail.type_id = 1
-                                			 LIMIT 1) AS gadaxdis_tarigi,
+                                	   LIMIT 1) AS gadaxdis_tarigi,
                                       (SELECT ROUND(SUM(IF(client_loan_agreement.loan_currency_id = 1,money_transactions_detail.pay_percent+money_transactions_detail.pay_amount,(money_transactions_detail.pay_percent*money_transactions_detail.course)+(money_transactions_detail.pay_amount*money_transactions_detail.course))),2)
                                        FROM   money_transactions
                                        JOIN   money_transactions_detail ON money_transactions.id = money_transactions_detail.transaction_id
@@ -65,7 +69,7 @@ switch ($action) {
                                        AND    money_transactions_detail.actived = 1
                                        AND    money_transactions_detail.`status` = 1
                                        AND    money_transactions_detail.type_id = 1
-                                			 LIMIT 1) AS gadaxdis_tarigi,
+                                	   LIMIT 1) AS gadaxdis_tarigi,
                                       (SELECT ROUND(SUM(IF(client_loan_agreement.loan_currency_id = 1,money_transactions_detail.pay_root,money_transactions_detail.pay_root*money_transactions_detail.course)),2)
                                        FROM   money_transactions
                                        JOIN   money_transactions_detail ON money_transactions.id = money_transactions_detail.transaction_id
@@ -79,7 +83,7 @@ switch ($action) {
                                 JOIN   client_loan_agreement ON client_loan_agreement.id = client_loan_schedule.client_loan_agreement_id
                                 JOIN   client ON client.id = client_loan_agreement.client_id
                                 WHERE  client_loan_schedule.actived = 1
-                                AND    (MONTH(client_loan_schedule.pay_date) = '$filt_month' OR (client_loan_schedule.`status` = 0 AND MONTH(client_loan_schedule.pay_date) < '$filt_month') OR (MONTH(client_loan_schedule.pay_date) = '$filt_month1' AND DAY(client_loan_schedule.pay_date) = '1' AND DAY(client_loan_agreement.datetime) !='1'))
+                                AND    (MONTH(client_loan_schedule.pay_date) = '$filt_month' OR MONTH(client_loan_agreement.datetime) = '$filt_month' OR (client_loan_schedule.status = 0 AND MONTH(client_loan_schedule.pay_date) <='$filt_month') OR (MONTH(client_loan_schedule.pay_date) = '$filt_month1' AND DAY(client_loan_schedule.pay_date) = '1' AND DAY(client_loan_agreement.datetime) !='1') OR (MONTH(client_loan_schedule.pay_date) = '$filt_month2' AND DAY(client_loan_schedule.pay_date) = '1' AND DAY(client_loan_agreement.datetime) !='1'))
                                 AND    YEAR(client_loan_schedule.pay_date) <= '$filt_year'
                                 AND    client_loan_agreement.canceled_status = 0
                                 AND    client.actived = 1 $AND
