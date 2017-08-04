bfbd32a11c77883648f9b5890222cc4e006da9a7
adm <adm@adm-PC>
2017-08-04 16:40:51 +0400

    დფგჰდფგჰ

--- server-side/operations/transaction.action.php
+++ server-side/operations/transaction.action.php
@@ -151,7 +151,7 @@ switch ($action) {
                                 LEFT JOIN client_car ON client_car.client_id = client.id
                                 LEFT JOIN user_info ON user_info.user_id = money_transactions.user_id
                                 WHERE     money_transactions.actived = '1' AND money_transactions.id > 59  AND money_transactions.type_id=2
-                                ORDER BY  money_transactions.pay_datetime DESC");
+                                ORDER BY  money_transactions.pay_datetime DESC, money_transactions.datetime DESC");
 	
 	        $data = array("aaData"	=> array());
 	
