648e490ec54c9acd73394dc9fc77aaff4e4ae9e8
adm <adm@adm-PC>
2017-08-09 17:46:21 +0400

    dfgdfgdfg

--- server-side/operations/transaction_detail.action.php
+++ server-side/operations/transaction_detail.action.php
@@ -515,8 +515,7 @@ function Add1($tr_id, $hidde_id, $transaction_date, $pledge_or_other_payed, $ple
         
         mysql_query(" UPDATE  money_transactions_detail
                       JOIN    money_transactions ON money_transactions.id = money_transactions_detail.transaction_id
-                      JOIN    client_loan_schedule ON client_loan_schedule.id = money_transactions.client_loan_schedule_id
-                      JOIN    client_loan_agreement ON client_loan_agreement.id = client_loan_schedule.client_loan_agreement_id
+                      JOIN    client_loan_agreement ON client_loan_agreement.id = money_transactions.agreement_id
                       SET     money_transactions_detail.actived = 0,
                               money_transactions_detail.balance_transaction_id = '$tr_id'
                       WHERE   client_loan_agreement.client_id = '$client_id'
