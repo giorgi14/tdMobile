1bd3ab171e21ada46594f66e356bd2d3a7fcae25
adm <adm@10.0.253.205>
2017-12-27 11:11:46 +0400

    sdfgsd

--- server-side/operations/transaction.action.php
+++ server-side/operations/transaction.action.php
@@ -744,11 +744,12 @@ switch ($action) {
                                         FROM   client_loan_schedule
                                         JOIN   client_loan_schedule_deal ON client_loan_schedule_deal.schedule_id = client_loan_schedule.id 
                                         WHERE  client_loan_schedule.actived = 1 AND client_loan_schedule.`status` = 0
-                                        AND    client_loan_schedule.id = $res[id]
+                                        AND    client_loan_schedule.id = $res[id] 
     		                            LIMIT 1");
     		
     		if (mysql_num_rows($check_deal)==0) {
         		if ($type_id == 1 || $type_id == 0) {
+        		    
         		    $check_deal = mysql_query(" SELECT SUM(deals_detail.amount+deals_detail.penalty) AS deal_amount,
         		                                       deals_detail.id AS deal_id,
         		                                       client_loan_schedule_deal.deals_penalty,
@@ -765,11 +766,13 @@ switch ($action) {
                                 		        WHERE  client_loan_schedule.actived = 1 AND client_loan_schedule.`status` = 1 AND client_loan_schedule.`actived` = 1
                                 		        AND    deals_detail.pay_date<='$transaction_date' AND deals_detail.`status` = 0 AND deals_detail.`actived` = 1 AND client_loan_schedule_deal.deal_status=1
         		                                $filt
-                                                ORDER BY client_loan_schedule_deal.pay_date ASC
+                                                ORDER BY client_loan_schedule_deal.pay_date ASC 
                                                 LIMIT 1");
-        		    $res_deal = mysql_fetch_array($check_deal);
-        		    $check_deal1 = mysql_num_rows($check_deal);
-        		    if ($check_deal1 > 0) {
+                    //$check_deal1 = mysql_num_rows($check_deal);
+                    $res_deal = mysql_fetch_array($check_deal);
+        		    
+        		    if ($res_deal[deal_id] != '' && !empty($res_deal[deal_id]) &&  $res_deal[deal_id]!=null) {
+        		        
             		    if ($check_penalty[penalty_stoped]==1) {
         		            $penalty=$check_penalty[penalty];
         		        }else{
@@ -787,7 +790,7 @@ switch ($action) {
         		            }
         		        }
         		    }
-    		        
+    		       
         		    $data = array('status' => 1, 'schedule_date'=>$res[schedule_date], 'id' => $res[id],'pay_amount' => $res[root] + $res[percent] + $penalty+$other_amount+$res_deal[deal_amount], 'root' => $res[root], 'percent' => $res[percent], 'penalty' => $penalty, 'client_data' => client($res[client_id]), 'client_attachment_data' => client_attachment($res[agree_id], $res['client_id']), 'agrement_data' => client_loan_number($res[agrement_id]), 'currenc' => currency($res[loan_currency_id]),'pay_amount1' => $res1[pay_amount], 'root1' => $res1[pay_root], 'percent1' => $res1[pay_percent], 'penalty1' => $res1[pay_penalty], 'loan_pay_amount' => $loan_pay_amount, 'info_message' => $info_message, 'other_amount' => $other_amount, 'deal_amount' => $res_deal[deal_amount], 'deal_id' => $res_deal[deal_id]);
         		}
     		}else{
